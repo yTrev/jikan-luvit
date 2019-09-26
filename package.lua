f765a987275d79d12c12b80e37782ecb332b4434
Pingu <gabriellopescidade@gmail.com>
2019-09-25 21:12:07 -0300

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,12 @@
+return {
+  name = 'PinguRBLX/JikanLua',
+  version = '1.0',
+  description = 'JikanLua is a wrapper for unofficial MAL API, Jikan.',
+  private = true,
+  tags = {'lua', 'lit', 'luvit', 'myanimelist', 'jikan'},
+  license = 'MIT',
+  homepage = 'https://github.com/JikanLua',
+  files = {
+      '**.lua',
+  }
+}
