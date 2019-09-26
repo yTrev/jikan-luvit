de49f0e93767b99cb832a9d9248ccf1c0b054971
Pingu <gabriellopescidade@gmail.com>
2019-09-25 21:13:51 -0300

    Fix package
    Remove Private

--- package.lua
+++ package.lua
@@ -2,7 +2,6 @@ return {
   name = 'PinguRBLX/JikanLua',
   version = '1.0',
   description = 'JikanLua is a wrapper for unofficial MAL API, Jikan.',
-  private = true,
   tags = {'lua', 'lit', 'luvit', 'myanimelist', 'jikan'},
   license = 'MIT',
   homepage = 'https://github.com/JikanLua',
